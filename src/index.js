cc7a2fa845becf8f8af6d704c2039798b8957777
fuzhenn <fuzhen@maptalks.org>
2018-08-11 22:33:20 +0800

    fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import * as maptalks from 'maptalks';
 import { GridLayer } from './gridlayer';
 import GridCanvasRenderer from './renderer/canvas-renderer';
 import GridGLRenderer from './renderer/gl-renderer';
@@ -7,9 +6,7 @@ GridLayer.registerRenderer('canvas', GridCanvasRenderer);
 GridLayer.registerRenderer('gl', GridGLRenderer);
 
 GridLayer.mergeOptions({
-    'renderer' : (() => {
-        return maptalks.Browser.webgl ? 'gl' : 'canvas';
-    })()
+    'renderer' : 'gl'
 });
 
 export { GridLayer };
