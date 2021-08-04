a411db6264b009d0d043fe3ce61e32bccf99c6fd
fuzhenn <fuzhen@maptalks.org>
2021-08-04 16:41:59 +0800

    0.6.7

--- dist/maptalks.gridlayer.es.js
+++ dist/maptalks.gridlayer.es.js
@@ -1,5 +1,5 @@
 /*!
- * maptalks.gridlayer v0.6.6
+ * maptalks.gridlayer v0.6.7
  * LICENSE : MIT
  * (c) 2016-2021 maptalks.org
  */
@@ -3618,4 +3618,4 @@ GridLayer.mergeOptions({
 
 export { GridLayer };
 
-typeof console !== 'undefined' && console.log('maptalks.gridlayer v0.6.6, requires maptalks@<2.0.0.');
+typeof console !== 'undefined' && console.log('maptalks.gridlayer v0.6.7, requires maptalks@<2.0.0.');
